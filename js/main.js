8b2dd8444ba072168aae1e754a9626c8016970ce
kbo580 <61081989+kbo580@users.noreply.github.com>
2023-01-10 14:54:34 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,10 +6,40 @@ $(function(){
     // $('body').toggleClass('active');
   });
   
-  $('.nav-menu li a').on('click', function () {
+  $('.nav__link').on('click', function () {
       $('#show').removeClass('active');
       $('#nav').removeClass('active');
-      // $('body').removeClass('active');
+      $('.sp-header__right').toggleClass('active');
   });
+
+  $('.nav__link').on('click', function(){
+    var headerHight = $('.header').outerHeight(true);
+    var id =$(this).attr('href');
+    var pos = $(id).offset().top - headerHight; 
+    $('html, body').animate({'scrollTop': pos}, 300);
+  });
+
+
+//画面をスクロールしたらフェードイン
+$(window).scroll(function(){
+  if($(this).scrollTop()>300){
+    $('#toTop').fadeIn(400);
+  }
+  else{
+    $('#toTop').fadeOut(400);
+  }
+});
+
+  $('#toTop').on('click', function() {
+    $('html, body').animate({ 
+      scrollTop: 0 }, 300);
+     return false;
+   });
+
+
+
+
+
+
 
 });
